02e2dc2cb3ea7c71a0d01ecd5fc535c029cebd31
eldardamirov <eldardamirov75@mail.ru>
2018-05-09 13:41:29 +0300

    Initial Commit

---
+++ Weather.today!/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Weather.today!
+//
+//  Created by Эльдар Дамиров on 09.05.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
